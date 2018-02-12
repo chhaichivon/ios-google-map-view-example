ac5e72ac17e1b18854ead9f3e835343fe61420b7
chhaichivon <chhaivinchivon@gmail.com>
2018-02-13 02:46:05 +0700

    ios map view example

--- ios-map/ViewController.swift
+++ ios-map/ViewController.swift
@@ -7,19 +7,74 @@
 //
 
 import UIKit
+import GoogleMaps
+
+
+class VacationDestination: NSObject {
+    
+    let name: String
+    let location: CLLocationCoordinate2D
+    let zoom: Float
+    
+    init(name: String, location: CLLocationCoordinate2D, zoom: Float) {
+        self.name = name
+        self.location = location
+        self.zoom = zoom
+    }
+    
+}
+
 
 class ViewController: UIViewController {
 
+    var mapView: GMSMapView?
+    
+    var currentDestination: VacationDestination?
+    
+    let destinations = [VacationDestination(name: "Embarcadero Bart Station", location: CLLocationCoordinate2DMake(37.792905, -122.397059), zoom: 14), VacationDestination(name: "Ferry Building", location: CLLocationCoordinate2DMake(37.795434, -122.39473), zoom: 18), VacationDestination(name: "Coit Tower", location: CLLocationCoordinate2DMake(37.802378, -122.405811), zoom: 15), VacationDestination(name: "Fisherman's Wharf", location: CLLocationCoordinate2DMake(37.808, -122.417743), zoom: 15), VacationDestination(name: "Golden Gate Bridge", location: CLLocationCoordinate2DMake(37.807664, -122.475069), zoom: 13)]
+
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let camera = GMSCameraPosition.camera(withLatitude: 11.5690331, longitude: 104.8886108, zoom: 20)
+        mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
+        view = mapView
+        
+        let currentLocation = CLLocationCoordinate2DMake(37.621262, -122.378945)
+        let marker = GMSMarker(position: currentLocation)
+        marker.title = "SFO Airport"
+        marker.map = mapView
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Next", style: .plain, target: self, action: Selector(("next")))
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func next() {
+        
+        if currentDestination == nil {
+            currentDestination = destinations.first
+        } else {
+            if let index = destinations.index(of: currentDestination!), index < destinations.count - 1 {
+                currentDestination = destinations[index + 1]
+            }
+        }
+        
+        setMapCamera()
     }
-
-
+    
+    private func setMapCamera() {
+        CATransaction.begin()
+        CATransaction.setValue(2, forKey: kCATransactionAnimationDuration)
+        mapView?.animate(to: GMSCameraPosition.camera(withTarget: currentDestination!.location, zoom: currentDestination!.zoom))
+        CATransaction.commit()
+        
+        let marker = GMSMarker(position: currentDestination!.location)
+        marker.title = currentDestination?.name
+        marker.map = mapView
+    }
+    
 }
 
